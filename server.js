6bf2b3dc0545229effbdf4aae8a8dee15c4ee385
peeyush1328 <peeyushkumar890@gmail.com>
2022-08-25 16:36:43 +0530

    created server with router.

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const http = require("http");
 const port = 8080;
 
+const todolist = ["cricket", "football", "baseball"];
+
 http
   .createServer((req, res) => {
-    res.writeHead(201, { "content-type": "text/html" });
-    res.write("<h1>Peeyush's servers yo yo banta repper</h1>");
+    const { method, url } = req;
+    if (url === "/list") {
+      if (method === "GET") {
+        res.writeHead(200, { "content-type": "text/html" });
+        res.write(todolist.toString());
+      } else if (method === "POST") {
+        let body = "";
+        req
+          .on("error", (err) => {
+            console.error(err);
+          })
+          .on("data", (chunks) => {
+            body = body + chunks;
+          })
+          .on("end", () => {
+            body = JSON.parse(body);
+          });
+      }
+    } else {
+      res.writeHead(404);
+    }
     res.end();
   })
 
