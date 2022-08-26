3d7679f4a9638c5dec6a61bd2b89f1d86182e8c8
peeyush1328 <peeyushkumar890@gmail.com>
2022-08-26 16:03:01 +0530

    created server using different METHODS

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ http
       if (method === "GET") {
         res.writeHead(200, { "content-type": "text/html" });
         res.write(todolist.toString());
+        console.log(todolist);
       } else if (method === "POST") {
         let body = "";
         req
@@ -21,6 +22,34 @@ http
           })
           .on("end", () => {
             body = JSON.parse(body);
+            let newtodo = todolist;
+            newtodo.push(body.item);
+            console.log(newtodo);
+            res.writeHead(201);
+          });
+      } else if (method === "DELETE") {
+        let body = "";
+        req
+          .on("error", (err) => {
+            console.error(err);
+          })
+          .on("data", (chunks) => {
+            body = body + chunks;
+          })
+          .on("end", () => {
+            body = JSON.parse(body);
+            let deletethis = body.item;
+            todolist.find((element, index) => {
+              if (element === deletethis) {
+                todolist.splice(index, 1);
+              }
+            });
+            // for (let i = 0; i < todolist.length; i++) {
+            //   if (todolist[i] === deletethis) {
+            //     todolist.splice(i, 1);
+            //   }
+            // }
+            res.writeHead(201);
           });
       }
     } else {
